91060564c201c9a1861775d640c45677b5b72be1
sachin <rishabhgit1704@gmail.comin>
2024-05-17 14:36:38 +0530

    Added all the src files

---
+++ src/components/array.js
@@ -0,0 +1,22 @@
+const playerdetails = [
+    {
+        id: "1",
+        name: "Virat Kohli",
+        Age: "32"
+    },
+    {
+        id: "2",
+        name: "Rohit Sharma",
+        Age: "33"
+    },{
+        id: "3",
+        name: "Shubhmann Gill",
+        Age: "25"
+    },{
+        id: "3",
+        name: "KL Rahul",
+        Age: "28"
+    },
+];
+
+export default playerdetails;
